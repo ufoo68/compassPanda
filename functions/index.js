0d52e1f7c0d025d8db099a51c7df9973b5cd6de9
ufoo68 <mitarou.yuutarou@gmail.com>
2019-05-03 13:27:53 +0900

    liffとの連携を実装

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,17 @@ const line = require('@line/bot-sdk');
 
 var arr = [ "魔材ンゴwwww", "やめたらこの仕事？", "歪みねえな", "24歳です" ];
 
+const liff = functions.config().liff.url;
+
 const config = {
-    channelSecret: '075845b961dd3f445ea608978bfb88bc',
-    channelAccessToken: 'HIrkVy4OssFQG5zxw503B8/HQ5HO3P4XJr3XUxt5bJ3HcYXn/VRO/deHl1x1Wprm0Qg7yKaH+vi22PCtwRLFVUO+0SyFszllXqCIa3LQbEbSWSZI8OUXbYyF7YWFGuSVAooqL46+dc7hW72Wbotp4gdB04t89/1O/w1cDnyilFU='
+    channelSecret: functions.config().channel.secret,
+    channelAccessToken: functions.config().channel.accesstoken
 };
 
 const app = express();
 
 app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(config);
     console.log(req.body.events);
     Promise
       .all(req.body.events.map(handleEvent))
@@ -23,7 +26,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 const client = new line.Client(config);
 
 async function handleEvent(event) {
-  if (event.message.type == 'location') {
+  if (event.message.type === 'location') {
     return client.replyMessage(event.replyToken, {
       type: 'sticker',
       packageId: 11538,
@@ -31,11 +34,15 @@ async function handleEvent(event) {
     });
   }
 
-  if (event.message.type == 'text') {
-    return client.replyMessage(event.replyToken, {
+  if (event.message.type === 'text') {
+    let reply = {
       type: 'text',
-      text: arr[ Math.floor( Math.random() * arr.length ) ]
-    });
+      text:  arr[ Math.floor( Math.random() * arr.length ) ]
+    }
+    if (event.message.text === 'liff') {
+      reply.text = liff;
+    }
+    return client.replyMessage(event.replyToken, reply);
   }
 
   if (event.type !== 'message' || event.message.type !== 'text') {
