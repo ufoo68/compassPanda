7d56dc94a7b81ed3745a391deb5f6d399080c132
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2019-08-02 12:53:42 +0900

    [change]リファクタリング

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,13 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const line = require('@line/bot-sdk');
-const admin = require('firebase-admin');
 
-admin.initializeApp(functions.config().firebase);
-
-var db = admin.firestore();
-
+const phraseBegin = ['友達登録ありがとう。', '私は今あなたがいる場所に呟きを残すことができるよ。', 'むふふ、すごいでしょ～。'];
 const phraseDef = [ 'んー、お姉さん難しいことわかんないなー', '君は面白いことを言うね', 'いいんだよ？たまには褒めてくれても',
               'いやー、君も飽きないね', 'どうやら楽しんでくれてるようで、お姉さんも嬉しいな', '安心してくれたまえよ君、明日は晴れだよ。たぶん'];
 const phraseOld = 'いいかい？私はかわいい。ならそれだけでいいじゃないか、ね？';
 const phraseLike = 'ん？好きなもの？それはもちろんキミニキマッテルジャナイカー';
+const phraseHotouse = ['どうすればいいか説明するよ。', 'つぶやきたいときは「つぶやきを残す」をタップしてフォームから記入してね。', '地図が見たいときは下のメニューから「地図を見る」をタップすると見れるよ。'];
 
 const config = {
     channelSecret: functions.config().channel.secret,
@@ -29,65 +26,44 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 
+function buildReplyText(text) {
+  return {
+    type: 'text',
+    text: text
+  }
+}
+
 async function handleEvent(event) {
-  let replyText1 = {
-    type: 'text',
-    text: ''
-  };
-  let replyText2 = {
-    type: 'text',
-    text: ''
-  };
-  let replyText3 = {
-    type: 'text',
-    text: ''
-  };
 
   // switch of follow
   switch (event.type) {
     case 'follow':
-        replyText1.text = `友達登録ありがとう。`;
-        replyText2.text = `私は今あなたがいる場所に呟きを残すことができるよ。`;
-        replyText3.text = `むふふ、すごいでしょ～。`;
-        return client.replyMessage(event.replyToken, [replyText1, replyText2, replyText3]);
+        return client.replyMessage(event.replyToken, [
+          buildReplyText(phraseBegin[0]),
+          buildReplyText(phraseBegin[1]),
+          buildReplyText(phraseBegin[2])
+        ]);
   }
 
   // switch of messages
   switch (event.message.type) {
-    case 'location':
-      let locateData = {};
-      locateData[event.timestamp] = {
-        latitude: event.message.latitude,
-        longitude:  event.message.longitude
-      };
-      replyText1.text = 'その場所ならこれかな？'
-      replyText2.text = `${event.timestamp}`;
-      replyText3.text = `上の番号を使って呟きを残してね`
-      db.collection('locateTweet').doc(`data`).set(locateData, { merge: true });
-      return client.replyMessage(event.replyToken, [replyText1, replyText2, replyText3]);
-    
     case 'text':
-      if (event.message.text.includes('歳') || event.message.text.includes('才')) replyText1.text = phraseOld;
-      else if (event.message.text.includes('好きなもの')) replyText1.text = phraseLike;
-      else replyText1.text = phraseDef[ Math.floor( Math.random() * phraseDef.length ) ];
-      const tweet = event.message.text.split(':');
-      if (tweet.length === 2) {
-        let tweetData = {};
-        tweetData[tweet[0]] = {
-          tweet: tweet[1]
-        };
-        db.collection('locateTweet').doc(`data`).set(tweetData, { merge: true });
-        replyText1.text = `タイムスタンプ「${tweet[0]}」で呟いたよ`;
+      if (event.message.text.includes('歳') || event.message.text.includes('才')) {
+        return client.replyMessage(event.replyToken, buildReplyText(phraseOld));
       }
-      switch (event.message.text) {
-        case '使い方':
-          replyText1.text = `どうすればいいか説明するよ。`;
-          replyText2.text = `呟きたいときは「位置情報」を教えてね。そしたらお姉さんが秘密のコードを教えてあげるから、呟きたいことを教えてね。`;
-          replyText3.text = `地図が見たいときは下のメニューから「地図を見る」を選んでね。`;
-          return client.replyMessage(event.replyToken, [replyText1, replyText2, replyText3]);
+      else if (event.message.text.includes('好きなもの')) {
+        return client.replyMessage(event.replyToken, buildReplyText(phraseLike));
       }
-      return client.replyMessage(event.replyToken, replyText1);
-      
+      else if (event.message.text.includes('使い方')) {
+        return client.replyMessage(event.replyToken, [
+          buildReplyText(phraseHotouse[0]),
+          buildReplyText(phraseHotouse[1]),
+          buildReplyText(phraseHotouse[2])
+        ]);
+      }
+      else {
+        return client.replyMessage(event.replyToken, buildReplyText(phraseDef[ Math.floor( Math.random() * phraseDef.length )]));
+      }
     default:
       return Promise.resolve(null);
   }
